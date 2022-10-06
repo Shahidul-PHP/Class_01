621707aed01c1e2d750dc940ac40c0bec5a1ae84
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-06 08:22:13 +0600

    class 01

---
+++ Practice.php
@@ -0,0 +1,29 @@
+<?php 
+
+echo 'CIT Web 2205 First Class Practice day';
+
+echo '</br>';
+echo '</br>';
+
+$a = 40;
+$b = 85;
+
+$c = $a + $b;
+
+
+
+echo "The value of".' $a is '.' '.$a;
+echo '</br>';
+
+echo "The value of".' $b is '.' '.$b;
+echo '</br>';
+
+echo 'Their Result Is'.' '.$c;
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+
+echo "hello world \n";
+
+echo "we are good in php \n";
+
+echo "this is a blank line";
+
+
+
+
+
+
+
+?>
